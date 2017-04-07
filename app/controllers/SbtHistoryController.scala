6e8a8ddf8103178093f78998c9e17ececb355ed5
Kyle Unverferth <kyle@kyleu.com>
2017-04-06 20:25:03 -0600

    UI improvements.

--- app/controllers/SbtHistoryController.scala
+++ app/controllers/SbtHistoryController.scala
@@ -10,7 +10,7 @@ import scala.concurrent.Future
 class SbtHistoryController @javax.inject.Inject() (override val app: Application, githubService: GithubService) extends BaseController {
   def list() = act(s"sbt.history") { implicit request =>
     val result = SbtHistoryService.list()
-    Future.successful(Ok(views.html.sbt.history(result)))
+    Future.successful(Ok(views.html.history.list(result)))
   }
 
   def compare() = act(s"sbt.history.compare") { implicit request =>
@@ -20,7 +20,7 @@ class SbtHistoryController @javax.inject.Inject() (override val app: Application
         val lSeq = SbtHistoryService.read(lName)
         val rSeq = SbtHistoryService.read(rName)
         val keys = (lSeq.map(_._1) ++ rSeq.map(_._1)).distinct.sorted
-        Future.successful(Ok(views.html.sbt.historyCompare(keys, lName, lSeq.toMap, rName, rSeq.toMap)))
+        Future.successful(Ok(views.html.history.compare(keys, lName, lSeq.toMap, rName, rSeq.toMap)))
       case _ => throw new IllegalStateException(s"Invalid [q] param: [${files.mkString(", ")}].")
     }
 
@@ -33,7 +33,7 @@ class SbtHistoryController @javax.inject.Inject() (override val app: Application
 
   def detail(key: String) = act(s"sbt.history.detail") { implicit request =>
     val result = SbtHistoryService.read(key)
-    Future.successful(Ok(views.html.sbt.historyDetail(key, result)))
+    Future.successful(Ok(views.html.history.detail(key, result)))
   }
 
   def delete(key: String) = act(s"sbt.history.delete") { implicit request =>
