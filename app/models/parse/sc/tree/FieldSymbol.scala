a664182ccb75a0d55db434c7029d00a4ba9b4ef2
Kyle Unverferth <kyle@kyleu.com>
2017-11-08 13:59:47 -0700

    Vals for all-caps identifiers.

--- app/models/parse/sc/tree/FieldSymbol.scala
+++ app/models/parse/sc/tree/FieldSymbol.scala
@@ -4,8 +4,21 @@ import models.parse.sc.transform.Transformations
 
 class FieldSymbol(prot: Boolean, nme: Name, readonly: Boolean) extends Symbol(nme) with JSNameable {
   var tpe: TypeRef = TypeRef.Any
+
   def tpeTranslated = tpe.copy(typeName = Transformations.forName(tpe.typeName))
-  val p = if (prot) { "protected " } else { "" }
-  val decl = if (readonly) { "val" } else { "var" }
+
+  val p = if (prot) {
+    "protected "
+  } else {
+    ""
+  }
+  val decl = if (readonly) {
+    "val"
+  } else if (name.name == name.name.toUpperCase) {
+    "val"
+  } else {
+    "var"
+  }
+
   override def toString = s"$jsNameStr$p$decl $name: $tpeTranslated"
 }
